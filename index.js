1074655ee2f4ab2a65f45e7a782523195e7ce2b2
brahmansh91 <brahmansh25112006@gmail.com>
2025-11-12 06:14:00 -0800

    Add server

---
+++ index.js
@@ -0,0 +1,25 @@
+require('dotenv').config()
+const express = require('express')
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get(`/login`,(req,res) => {
+    res.send("brahmansh@91")
+})
+
+app.get(`/youtube`,(req,res) => {
+    res.send('<h1> YOUNG ANSH </h1>')
+})
+
+//get is a type of request given through express here.
+
+app.listen(port, () => {
+  console.log(`anshu backend ${port}`)
+})
+
+//express listen the request that sended. 
